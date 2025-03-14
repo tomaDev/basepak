5f91f08ba2f4b8994b9d3990b2aca22cf40cd0fc
tomaDev <46246321+tomaDev@users.noreply.github.com>
2025-03-15 01:00:09 +0200

    adjust console configs for NO_COLOR (#23)

--- src/basepak/__init__.py
+++ src/basepak/__init__.py
@@ -1,1 +1,10 @@
-__version__ = '0.0.30'
+__version__ = '0.0.31'
+
+import os
+if os.environ.get('NO_COLOR'):
+    import click
+
+    def no_color_style(text, *args, **kwargs):  # noqa unused
+        return text
+
+    click.style = no_color_style
